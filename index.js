f45dab15b06327cc873e1354335d5b2376e34ce3
Max <tolstikov_m@mail.ru>
2019-04-05 19:02:12 +0300

    add style tag

--- index.js
+++ index.js
@@ -1,24 +1,18 @@
 ;(function(){
 
+    let head = document.head
+    let body = document.body
 
 let plugin_wrapper = document.createElement('div')
+body.appendChild(plugin_wrapper)
 
-let style_tag = document.createElement('style')
-style_tag.type = 'text/css'
-let head = document.head
+let style_tag = document.createElement('link')
+style_tag.href = 'style.css'
+style_tag.rel = 'stylesheet'
+
+
 head.appendChild(style_tag)
 
-let css = require('./style.css')
 
-// import css from "./style.css"
-
-console.log('css', css)
-
-if (style_tag.styleSheet){
-    // style_tag.styleSheet.cssText = css
-}
-else {
-    // style_tag.appendChild(document.createTextNode(css))
-}
 
 }())
