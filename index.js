bceb844622a5dac3774504190ee5057745ba3f81
Max <tolstikov_m@mail.ru>
2019-04-08 14:49:35 +0300

    Create template of plugin

--- index.js
+++ index.js
@@ -1,17 +1,118 @@
 ;(function(){
 
-    let head = document.head
-    let body = document.body
 
-let plugin_wrapper = document.createElement('div')
-body.appendChild(plugin_wrapper)
+    let initRenderPlugin = function() {
 
-let style_tag = document.createElement('link')
-style_tag.href = 'style.css'
-style_tag.rel = 'stylesheet'
+        let head = document.head
+        let body = document.body
 
 
-head.appendChild(style_tag)
+        let plugin_wrapper = document.createElement('div')
+        plugin_wrapper.id = 'test-interface-imitator-wrapeer'
+
+        body.appendChild(plugin_wrapper)
+
+
+        // create header
+        let img_toggle_position = document.createElement('img')
+        img_toggle_position.src = './images/scroll-vertical-arrows.png'
+        img_toggle_position.id = 'tii-img-toggle-position'
+        let image_toggle_position_wrapper = document.createElement('div')
+        image_toggle_position_wrapper.classList.add('tii-img-toggle-position-wrapper')
+        
+        image_toggle_position_wrapper.appendChild(img_toggle_position)
+
+        let header_wrapper = document.createElement('div')
+        header_wrapper.classList.add('tii-header')
+        plugin_wrapper.appendChild(header_wrapper)
+
+        header_wrapper.appendChild(image_toggle_position_wrapper)
+
+
+
+        // create body
+        let body_wrapper = document.createElement('div')
+        body_wrapper.classList.add('tii-body-wrapper')
+        plugin_wrapper.appendChild(body_wrapper)
+
+        // create include input
+        let include_input_wrapper = document.createElement('div')
+        include_input_wrapper.classList.add('tii-include-input-wrapper')
+        body_wrapper.appendChild(include_input_wrapper)
+        let include_label = document.createElement('label')
+        include_label.for = 'tii-include-elements'
+        include_label.innerHTML = 'Include elements'
+        include_input_wrapper.appendChild(include_label)
+        let include_input = document.createElement('input')
+        include_input.name = include_input.id = 'tii-include-elements'
+        include_input.placeholder = 'Enter elements'
+        include_input_wrapper.appendChild(include_input)
+
+        // create exclude input
+        let exclude_input_wrapper = document.createElement('div')
+        exclude_input_wrapper.classList.add('tii-exclude-input-wrapper')
+        body_wrapper.appendChild(exclude_input_wrapper)
+        let exclude_label = document.createElement('label')
+        exclude_label.for = 'tii-exclude-elements'
+        exclude_label.innerHTML = 'Exclude elements'
+        exclude_input_wrapper.appendChild(exclude_label)
+        let exclude_input = document.createElement('input')
+        exclude_input.name = exclude_input.id = 'tii-exclude-elements'
+        exclude_input.placeholder = 'Enter elements'
+        exclude_input_wrapper.appendChild(exclude_input)
+
+        // create timers wrapper
+        let timers_wrapper = document.createElement('div')
+        timers_wrapper.classList.add('tii-timers-wrapper')
+        body_wrapper.appendChild(timers_wrapper)
+
+        // create intervar input
+        let interval_input_wrapper = document.createElement('div')
+        interval_input_wrapper.classList.add('tii-interval-input-wrapper')
+        timers_wrapper.appendChild(interval_input_wrapper)
+        let interval_label = document.createElement('label')
+        interval_label.for = 'tii-interval-input'
+        interval_label.innerHTML = 'Interval run listners'
+        interval_input_wrapper.appendChild(interval_label)
+        let interval_input = document.createElement('input')
+        interval_input.name = interval_input.id = 'tii-interval-input'
+        interval_input.type = 'number'
+        interval_input_wrapper.appendChild(interval_input)
+
+        // create total duration input
+        let total_duration_input_wrapper = document.createElement('div')
+        total_duration_input_wrapper.classList.add('tii-total-duration-input-wrapper')
+        timers_wrapper.appendChild(total_duration_input_wrapper)
+        let total_duration_label = document.createElement('label')
+        total_duration_label.for = 'tii-total-duration-input'
+        total_duration_label.innerHTML = 'Total duration test'
+        total_duration_input_wrapper.appendChild(total_duration_label)
+        let total_duration_input = document.createElement('input')
+        total_duration_input.name = total_duration_input.id = 'tii-total-duration-input'
+        total_duration_input.type = 'number'
+        total_duration_input_wrapper.appendChild(total_duration_input)
+
+        // create button start test
+        let button_wrapper = document.createElement('div')
+        button_wrapper.classList.add('tii-button-wrapper')
+        body_wrapper.appendChild(button_wrapper)
+        let button_start = document.createElement('button')
+        button_start.classList.add('tii-button')
+        button_start.innerHTML = 'Start test'
+        button_wrapper.appendChild(button_start)
+
+
+
+        let style_tag = document.createElement('link')
+        style_tag.href = 'style.css'
+        style_tag.rel = 'stylesheet'
+
+
+        head.appendChild(style_tag)
+
+    }
+
+    initRenderPlugin()
 
 
 
